51dc16a9fdb9bfa0733af3f102b2e0b2ad0f84ff
Caroline Kalorkoti <carolineecrandell@gmail.com>
2019-07-01 12:06:16 -0700

    Incorporating Foundation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,50 +1,52 @@
-$(document).ready(function () {
+$( document ).ready(function() {
+$(document).foundation();
 
-    var keys = {
-        placesApi: "AIzaSyBGBzQZ59oXxaYkhLWHOjPf_Jm4Ez_p4E4"
-    }
+var keys = {
+    placesApi: "AIzaSyBXO25bppW7obhCSe-wix0epBJ1zlffMGA"
+}
 
-    $("#zip-code").on("click", function (event) {
-        var zip = $("#zip-input").val().trim();
-        event.preventDefault();
-        console.log(zip);
-        $("#display-places").empty();
+$("#zip-butt").on("click", function (event) {
+    var zip = $("#zip-input").val().trim();
+    event.preventDefault();
+    console.log(zip);
+    $("#display-places").empty();
 
-        function queryPlaces(searchType, zip) {
-            var queryString = $.param({
-                key: keys.placesApi,
-                query: searchType + " in " + zip,
-                fields: "photos,formatted_address,name,rating,opening_hours,geometry"
-            });
-            var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?" + queryString;
-            return $.ajax({
-                method: "GET",
-                url: queryUrl
-            });
-        }
-        var requests = [];
-        requests.push(queryPlaces("attraction", zip));
-        requests.push(queryPlaces("restaurant", zip));
-        var doAllRequests = Promise.all(requests);
-    
-        doAllRequests.then(function (responses) {
-            console.log(responses);
-            for (var i = 0; i < responses.length; i++) {
-                for (var j = 0; j < responses[i].results.length; j++) {
-                    var $newDiv = $("<div>");
-                    var $name = $("<h1>");
-                    var $placePic = $("<p>");
-                    var $image = $("<img>");
-                    $name.html(responses[i].results[j].name);
-                    $placePic.html(responses[i].results[j].formatted_address);
-                    var photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxheight=150&photoreference=" + responses[i].results[j].photos[0].photo_reference + "&key=AIzaSyBe_wSwxi8DiMJ4g5ClD0ItuTGWWWxcLlg"
-                    $image.attr("src",photoUrl);
-                    $newDiv.append($name);
-                    $newDiv.append($placePic);
-                    $newDiv.append($image);
-                    $("#display-places").append($newDiv);
-                }
-            }
+    function queryPlaces(searchType, zip) {
+        var queryString = $.param({
+            key: keys.placesApi,
+            query: searchType + " in " + zip,
+            fields: "photos,formatted_address,name,rating,opening_hours,geometry"
         });
+        var queryUrl = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?" + queryString;
+        return $.ajax({
+            method: "GET",
+            url: queryUrl
+        });
+    }
+    var requests = [];
+    requests.push(queryPlaces("attraction", zip));
+    requests.push(queryPlaces("restaurant", zip));
+    var doAllRequests = Promise.all(requests);
+
+    doAllRequests.then(function (responses) {
+        console.log(responses);
+        for (var i = 0; i < responses.length; i++) {
+            for (var j = 0; j < responses[i].results.length; j++) {
+                var $newDiv = $("<div>");
+                var $name = $("<p>");
+                var $placeDes = $("<p>");
+                var $image = $("<img>");
+                $name.html(responses[i].results[j].name);
+                $placeDes.html(responses[i].results[j].formatted_address);
+                var photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxheight=150&photoreference=" + responses[i].results[j].photos[0].photo_reference + "&key=AIzaSyBXO25bppW7obhCSe-wix0epBJ1zlffMGA"
+                $image.attr("src", photoUrl);
+                $newDiv.append($placeDes);
+                $newDiv.append($name);
+                $newDiv.append($image);
+                $("#display-places").append($newDiv);
+
+            }
+        }
     });
+});
 });
