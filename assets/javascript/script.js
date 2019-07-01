feaef39b7290f84b9df6c33d2aa8ddccc66dc001
Caroline Kalorkoti <carolineecrandell@gmail.com>
2019-07-01 13:32:01 -0700

    Foundation cards

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -30,21 +30,24 @@ $("#zip-butt").on("click", function (event) {
 
     doAllRequests.then(function (responses) {
         console.log(responses);
+        $cards = $("<div class='row small-up-1 medium-up-2 large-up-3'>");
         for (var i = 0; i < responses.length; i++) {
             for (var j = 0; j < responses[i].results.length; j++) {
-                var $newDiv = $("<div>");
-                var $name = $("<p>");
-                var $placeDes = $("<p>");
+                var $newDiv = $("<div class='column'>");
+                var $newCall = $("<div class='callout'>");
+                var $name = $("<p class='lead'>");
+                var $placeDes = $("<p class='subheader'>");
                 var $image = $("<img>");
                 $name.html(responses[i].results[j].name);
                 $placeDes.html(responses[i].results[j].formatted_address);
                 var photoUrl = "https://maps.googleapis.com/maps/api/place/photo?maxheight=150&photoreference=" + responses[i].results[j].photos[0].photo_reference + "&key=AIzaSyBXO25bppW7obhCSe-wix0epBJ1zlffMGA"
                 $image.attr("src", photoUrl);
-                $newDiv.append($placeDes);
-                $newDiv.append($name);
-                $newDiv.append($image);
-                $("#display-places").append($newDiv);
-
+                $newCall.prepend($placeDes);
+                $newCall.prepend($name);
+                $newCall.prepend($image);
+                $newDiv.append($newCall);
+                $cards.append($newDiv);
+                $("#display-places").append($cards);
             }
         }
     });
